b8ccd5e9d6bc5797ffb3dc250f0107aea3b9caeb
Vee Upatising <veeupatising@gmail.com>
2018-12-07 00:17:17 -0500

    Add files via upload

---
+++ Face.java
@@ -0,0 +1,18 @@
+/**
+ * Created by starb on 4/30/2018.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Face extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
---
+++ FaceDetection.java
@@ -0,0 +1,5 @@
+/**
+ * Created by starb on 4/30/2018.
+ */
+public class FaceDetection {
+}
